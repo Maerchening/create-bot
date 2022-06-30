621104748ac2a3bfdb0c5a1ec8b68eb828f8e150
Maerchening <mujin9902@gmail.com>
2022-06-30 17:52:56 +0900

    create

---
+++ bot.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+import discord
+import asyncio
+from json import loads
+
+
+client_id = "4tn3n6xydw7yqfg5imjga6p445xc98"
+client_secret = "a0ps9p2blz1pcqhqxz8qm36irwz9nt"
+discord_Token = "OTkxNDY2MjczNjA5MzYzNDU3.GBIyZt.Kna8ZAZjf49ld7A_1vN034fy0RnSXwXfJ9y0NU"
+
+discord_ID = "858731610753335316"
+discord_state = "기다리는중~"
+
+Twitch_ID = "옥쓔"
+ment = "시간 나면 한번 들러줘"
+
+
+print("good")
