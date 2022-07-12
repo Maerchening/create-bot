14be0d303ccbec018022b16c4d8a40cf38fab38a
Maerchening <mujin9902@gmail.com>
2022-07-13 05:47:00 +0900

    can't try

--- bot.py
+++ bot.py
@@ -4,41 +4,48 @@ import discord
 import asyncio
 from json import loads
 
-
 client_id = "4tn3n6xydw7yqfg5imjga6p445xc98"
-client_secret = "a0ps9p2blz1pcqhqxz8qm36irwz9nt"
-discord_Token = "OTkxNDY2MjczNjA5MzYzNDU3.GBIyZt.Kna8ZAZjf49ld7A_1vN034fy0RnSXwXfJ9y0NU"
+client_secret = "ngxtv8p34es4btueh30ee6f7n1zzok"
+discord_Token = ""
 
-discord_ID = "858731610753335316"
+discord_ID = "992003515163623446"
 discord_state = "기다리는중~"
 
-Twitch_ID = "옥쓔"
+Twitch_ID = 'bggul'
 ment = "시간 나면 한번 들러줘"
 
 client = discord.Client()
 
 @client.event
+
 async def on_ready():
-    print(client.userid)
+
+    print(client.user.id)
     print("ready")
-    
     game = discord.Game(discord_state)
-    await client.change_presence(discord.Status.online, activity=game)
-    
-    channel = client.get_all_channel(discord_ID)
-    
-    oauth_key = requests.post("https://id.twitch.tv/oauth2/token?client_id=" + client_id + "&client_secret=" + client_secret + "&arant_type=client_credentials")
+    await client.change_presence(status=discord.Status.online, activity=game)
+    channel = client.get_channel(discord_ID)
+    oauth_key = requests.post("https://id.twitch.tv/oauth2/token?client_id=" + client_id + "&client_secret=" + client_secret + "&grant_type=client_credentials")
     access_token = loads(oauth_key.text)["access_token"]
-    token_type = 'Bearer'
-    autorization = token_type + access_token
-    print(autorization)
-    
+    token_type = 'Bearer '
+    authorization = token_type + access_token
+    print(authorization)
     check = False
-    
+
     while True:
         print("ready on Notification")
-        
-        
+        headers = {'client-id': client_id, 'Authorization': authorization}
+        response_channel = requests.get('https://api.twitch.tv/helix/streams?user_login=' + Twitch_ID, headers=headers)
+        print(response_channel.text)
+        try:
+            if loads(response_channel.text)['data'][0]['type'] == 'live':
+                print('good')
+                await channel.send('ment + \n https://www.twitch.tv/ + Twitch_ID')
+                print("Online")
+                check = True
+        except:
+            print("Offline")
+            check = False
+        await asyncio.sleep(30)
 
-
-print("good")
+client.run(discord_Token)
