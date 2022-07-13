dc3490932ffc51e261d60055005f3284e1f4c38e
Maerchen <mujin9902@gmail.com>
2022-07-13 09:16:33 +0900

    최종

--- README.txt
+++ README.txt
@@ -1,4 +1,47 @@
 디스코드 봇 만들기
 
-1. 봇 연동
-2. github commit
+1. 토큰 및 인증기 받기
+client_id, client_secret = twtich개발자모드에서 받기
+
+discord_state = 디스코드에 표시되는 상태
+Twitch_ID = 알림을 받고싶은 스트리머 ID
+ment = 봇 알림과 같이 받고싶은 메시지
+
+oauth_key, access_token, token_type, authorization
+ - 트위치에서 인증을 받기 위한 설정
+
+2. 봇 만들기
+discord.Client()로 discord 접속
+
+on_ready()
+ - 봇이 실행
+
+discord.Game
+ - 봇의 상태를 설정
+
+await client.change_presence
+ - 봇의 상태를 변경
+
+channel
+ - 알림을 보낼 채널을 설정
+
+check
+ - 알림을 받았는지 확인
+
+headers
+ - 트위치 2차 인증을 받을 요소
+
+response_channel
+ - headers에서 가져온것들을 바탕으로 트위치에서 api인증을 받아서 방송 요소들을 받아옴
+
+try
+ - 방송 정보들을 받아서 live상태이고 알림을 받지 않은경우 실행
+
+await channel.send
+ - 설정한 채널에 메시지를 보냄
+
+except
+ - 오프라인인 경우
+
+await asyncio.sleep()
+ - ()안에 시간에 따라 while문 다시 실행
--- bot.py
+++ bot.py
@@ -1,51 +1,40 @@
 import requests
-from bs4 import BeautifulSoup
 import discord
 import asyncio
 from json import loads
 
-client_id = "4tn3n6xydw7yqfg5imjga6p445xc98"
-client_secret = "ngxtv8p34es4btueh30ee6f7n1zzok"
+client_id = ""
+client_secret = ""
 discord_Token = ""
 
-discord_ID = "992003515163623446"
-discord_state = "기다리는중~"
+discord_state = "대기"
 
-Twitch_ID = 'bggul'
-ment = "시간 나면 한번 들러줘"
+Twitch_ID = ''
+ment = "방송켰다~ 당장 달려와~"
 
 client = discord.Client()
 
 @client.event
-
 async def on_ready():
-
-    print(client.user.id)
-    print("ready")
     game = discord.Game(discord_state)
     await client.change_presence(status=discord.Status.online, activity=game)
-    channel = client.get_channel(discord_ID)
+    channel = client.get_channel()
     oauth_key = requests.post("https://id.twitch.tv/oauth2/token?client_id=" + client_id + "&client_secret=" + client_secret + "&grant_type=client_credentials")
     access_token = loads(oauth_key.text)["access_token"]
     token_type = 'Bearer '
     authorization = token_type + access_token
-    print(authorization)
     check = False
 
     while True:
-        print("ready on Notification")
         headers = {'client-id': client_id, 'Authorization': authorization}
         response_channel = requests.get('https://api.twitch.tv/helix/streams?user_login=' + Twitch_ID, headers=headers)
-        print(response_channel.text)
+        #print(response_channel.text)
         try:
-            if loads(response_channel.text)['data'][0]['type'] == 'live':
-                print('good')
-                await channel.send('ment + \n https://www.twitch.tv/ + Twitch_ID')
-                print("Online")
+            if loads(response_channel.text)['data'][0]['type'] == 'live' and check==False:
+                await channel.send(ment + '\nhttps://www.twitch.tv/' + Twitch_ID)
                 check = True
         except:
-            print("Offline")
             check = False
-        await asyncio.sleep(30)
+        await asyncio.sleep(10)
 
 client.run(discord_Token)
